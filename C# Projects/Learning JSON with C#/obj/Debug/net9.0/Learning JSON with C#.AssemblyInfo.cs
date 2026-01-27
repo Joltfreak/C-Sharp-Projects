b9fff000adac1fdc1d026d7739addd93987dc2c2
Joltfreak <joltfreak82@gmail.com>
2026-01-26 22:52:04 -0600

    Created C# Weather API App
    Created a weather API call to the Open-Meteo and learned about the differences between JsonNode and JsonSerializer.

--- C# Projects/Learning JSON with C#/obj/Debug/net9.0/Learning JSON with C#.AssemblyInfo.cs
+++ C# Projects/Learning JSON with C#/obj/Debug/net9.0/Learning JSON with C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Learning JSON with C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df8ca6329cbb21aa5f6f9d3ba4c6136617fb8c55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Learning JSON with C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Learning JSON with C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ C# Projects/Weather Forecast API Project/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+
+namespace Weather_Forecast_API_Project;
+
+class Program
+{
+    static bool ProgramRunning = true;
+    static string LAT = string.Empty;
+    static string LON = string.Empty;
+    static void Main()
+    {
+        while
+        (ProgramRunning)
+        {
+            Console.Clear();
+            Console.WriteLine("Do you want to check you local temperatue?");
+            Console.WriteLine("Enter Y for YES and N for NO");
+            string? userInput = Console.ReadLine();
+
+            if(userInput.ToLower() == "y")
+            {
+                Console.Write("Please enter a LATITUDE: ");
+                LAT = Console.ReadLine();
+                Console.Write("Please enter a LONGITUDE: ");
+                LON = Console.ReadLine();
+
+                GetWeatherData(LON, LAT).Wait();
+
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            else if(userInput.ToLower() == "n")
+            {
+                ProgramRunning = false;
+            }
+        }
+    }
+
+    static async Task GetWeatherData(string lon, string lat)
+    {
+        // create a new HTTP Client
+        using HttpClient client = new HttpClient();
+
+        try
+        {
+            // we then declare the URL
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m&temperature_unit=fahrenheit";
+
+            // we await a response from the URL
+            string jsonResponse = await client.GetStringAsync(url);
+            
+            // we use the JsonNode.Parse() because we need a dynamic like object.
+            // we could use something like JsonSerializer.Deserialize but that requires it intaking something like a Class.
+            var weatherData = JsonNode.Parse(jsonResponse);
+            var currentTemp = weatherData["hourly"]["temperature_2m"][0];
+            var tempUnit = weatherData["hourly"]["temperature_unit"];
+
+            Console.WriteLine($"Current Temperature: {currentTemp}{tempUnit}");
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
