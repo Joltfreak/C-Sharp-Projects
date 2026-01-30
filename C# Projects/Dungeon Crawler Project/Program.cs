299899b855f948eedcad88b1fa3a79502da3dcc6
Joltfreak <joltfreak82@gmail.com>
2026-01-30 11:38:30 -0600

    Just created project for text based dungeon crawler

---
+++ C# Projects/Dungeon Crawler Project/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Dungeon_Crawler_Project;
+
+class Program
+{
+    static void Main()
+    {
+
+    }
+
+    // goal is to make a random dungeon crawler that has possible some ASCII art. I plan on using REXPaint
+    // for creating the ASCII art. I may need to learn how to load PNGs.
+    // this project will be using Classes, turn based logic and more.
+    // for the beginning I want to start off by making it Console Based so just text.
+}
